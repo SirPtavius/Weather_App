081148b0589cbd9d005902047dcd2feb469421d3
Gabriele <124803038+SirPtavius@users.noreply.github.com>
2024-07-31 12:26:40 +0200

    improved Js and style. To fix: next days forecast disapear when press on one of the cards

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ submit.addEventListener("click", () => {
   getWeatherReport(location);
 });
 
+const input = document.querySelector(".city");
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    submit.click();
+  }
+});
+
 async function getWeatherReport(location) {
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${weather_API_key}`;
 
@@ -27,19 +35,153 @@ async function getWeatherReport(location) {
 
   UpdateWeatherUI(weatherData);
 }
-/*conditions, datetime, temp */
 
 function UpdateWeatherUI(weatherData) {
-  const condition = document.createElement("p");
-  condition.textContent = weatherData.currentConditions.conditions;
-  const dateTime = document.createElement("p");
-  dateTime.textContent = weatherData.currentConditions.datetime;
-  const temp = document.createElement("p");
-  temp.textContent = weatherData.currentConditions.temp;
-  /*
-  const weatherInfo = document.querySelector(".weatherInfo");
-  weatherInfo.appendChild(condition);
-  weatherInfo.appendChild(dateTime);
-  weatherInfo.appendChild(temp);
-  */
+  const dataCity = weatherData.resolvedAddress;
+
+  const dataCondition =
+    (weatherData.currentConditions &&
+      weatherData.currentConditions.conditions) ||
+    weatherData.conditions;
+  const dataDateTime =
+    (weatherData.currentConditions && weatherData.currentConditions.datetime) ||
+    weatherData.datetime;
+  const dataTemp =
+    (weatherData.currentConditions && weatherData.currentConditions.temp) ||
+    weatherData.temp;
+  const dataWind =
+    (weatherData.currentConditions &&
+      weatherData.currentConditions.windspeed) ||
+    weatherData.windspeed;
+  const dataDay =
+    (weatherData.days && weatherData.days[0].datetime) || weatherData.datetime;
+  const dataDescription = weatherData.description;
+
+  const leftCity = document.querySelector(".leftCity");
+  if (dataCity !== undefined) {
+    leftCity.textContent = dataCity;
+  }
+
+  const leftTemp = document.querySelector("#temperature");
+  leftTemp.textContent = dataTemp + "°C";
+
+  const wind = document.querySelector(".wind");
+  wind.textContent = dataWind + " km/h";
+
+  const leftTime = document.querySelector(".leftTime");
+  if (dataDateTime !== dataDay) {
+    leftTime.textContent = dataDateTime;
+  }
+  const leftDate = document.querySelector(".leftDate");
+  leftDate.textContent = dataDay;
+
+  const leftCondition = document.querySelector(".leftCondition");
+  leftCondition.textContent = dataCondition;
+
+  const leftDescription = document.querySelector(".leftDescritpion");
+  leftDescription.textContent = dataDescription;
+
+  const leftSctHour = document.querySelector(".leftSctHour");
+
+  //Create Left Section Card Hour
+
+  const arrayHourData =
+    (weatherData.days && weatherData.days[0].hours) || weatherData.hours;
+
+  leftSctHour.innerHTML = "";
+  arrayHourData.forEach((hours, hour) => {
+    const cardHourDiv = document.createElement("div");
+    cardHourDiv.className = "cardHour";
+
+    const timeHourP = document.createElement("p");
+    timeHourP.className = "timeHourSct";
+
+    timeHourP.textContent = hours.datetime.slice(0, -3);
+
+    const imgElement = document.createElement("img");
+
+    imgElement.src = `images/icon/${dataCondition}.png`;
+    imgElement.alt = `${dataCondition}`;
+    imgElement.className = "sidebarImg";
+
+    const tempHourP = document.createElement("p");
+    tempHourP.className = "temperaturehourSct";
+    tempHourP.textContent = hours.temp + "°C";
+
+    cardHourDiv.appendChild(timeHourP);
+    cardHourDiv.appendChild(imgElement);
+    cardHourDiv.appendChild(tempHourP);
+
+    leftSctHour.appendChild(cardHourDiv);
+  });
+
+  createNextDays(5);
+
+  //Add event Listener to 5/10/15 Days button
+  const forecastBtns = document.querySelectorAll(".nextDaysBtn");
+
+  forecastBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      createNextDays(parseInt(btn.textContent));
+    });
+  });
+
+  //Create Next Days Cards
+  function createNextDays(NDay) {
+    const arrayDaysData = weatherData.days;
+
+    const otherDaysCards = document.querySelector(".otherDaysCards");
+    otherDaysCards.innerHTML = "";
+    for (let i = 0; i < NDay; i++) {
+      const cardDiv = document.createElement("div");
+      cardDiv.className = "card";
+
+      const imgElement = document.createElement("img");
+      imgElement.src = `images/icon/${arrayDaysData[i].conditions}.png`;
+      imgElement.alt = `${arrayDaysData[i].conditions}`;
+      imgElement.className = "sidebarImg";
+
+      const infoDiv = document.createElement("div");
+
+      const dateP = document.createElement("p");
+      dateP.className = "date";
+      dateP.textContent = arrayDaysData[i].datetime;
+
+      const conditionP = document.createElement("p");
+      conditionP.className = "condition";
+      conditionP.textContent = arrayDaysData[i].conditions;
+
+      infoDiv.appendChild(dateP);
+      infoDiv.appendChild(conditionP);
+
+      const cardTempDiv = document.createElement("div");
+      cardTempDiv.className = "cardTemp";
+
+      const tempMinP = document.createElement("p");
+      tempMinP.className = "tempMin";
+      tempMinP.textContent = arrayDaysData[i].tempmin + "°C";
+
+      const tempMaxP = document.createElement("p");
+      tempMaxP.className = "tempMax";
+      tempMaxP.textContent = arrayDaysData[i].tempmax + "°C";
+
+      cardTempDiv.appendChild(tempMinP);
+      cardTempDiv.appendChild(tempMaxP);
+
+      cardDiv.appendChild(imgElement);
+      cardDiv.appendChild(infoDiv);
+      cardDiv.appendChild(cardTempDiv);
+
+      otherDaysCards.appendChild(cardDiv);
+
+      cardDiv.addEventListener("click", () => {
+        const index = weatherData.days.findIndex(
+          (day) => day.datetime === dateP.textContent
+        );
+
+        leftSctHour.innerHTML = "";
+        UpdateWeatherUI(weatherData.days[index]);
+      });
+    }
+  }
 }
